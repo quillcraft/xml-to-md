815a950b613358bb2ccf328db4483c79259fbf5c
Anton Mizinov <quillcraft@Antons-MacBook-Air.local>
2022-04-09 01:56:28 +0300

    Added main logic

--- converter.js
+++ converter.js
@@ -1,35 +1,64 @@
 const fs = require('fs')
+const { DOMParser, XMLSerializer } = require('@xmldom/xmldom')
+const html2md = require('html-to-md')
 
 const input = './xml'
 const output = './md'
 
-////////////////////////////////////////////////////////////////////////////////
-// Create Markdown files
+start()
 
-// function createMarkdown(data) {
-// 	try {
-// 		fs.writeFileSync(`${output}/${filename}`, md, { encoding: 'utf8' })
-// 		console.log(`Created file: ${filename}`)
+function getMarkdown(entry) {
+	const subject = entry.getElementsByTagName('subject')
+	const subjectNode = Array.from(subject)[0]
+	const title = subjectNode.textContent
 
-// 	} catch (error) {
-// 		console.error(error)
-// 	}
-// }
+	const event = entry.getElementsByTagName('event')
+	const eventNode = Array.from(event)[0]
+	const eventHTML = new XMLSerializer().serializeToString(eventNode)
+	const content = html2md(eventHTML, { skipTags: ['event'] })
 
-////////////////////////////////////////////////////////////////////////////////
-// Start converter
+	return `---\nlayout: post\ntitle: "${title}"\ncategories: event\n---\n${content}`
+}
 
-function start() {
+function writeFile(entry) {
 	try {
-		// fs.readFile(`${input}/${filename}`, 'utf8', (error, filedata) => {
-		// 	if (error) throw error
+		const eventtime = entry.getElementsByTagName('eventtime')
+		const timestamp = Array.from(eventtime)[0].textContent
+		const filename = timestamp.split(' ')[0]
+		const md = getMarkdown(entry)
 
-		// 	createMarkdown(filedata)
-		// })
+		fs.writeFileSync(`${output}/${filename}.md`, md, { encoding: 'utf8' })
 
+		console.log(`Created file: ${filename}.md`)
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+function readFile(file) {
+	try {
+		fs.readFile(`${input}/${file}`, 'utf8', (error, xml) => {
+			if (error) throw error
+
+			const parser = new DOMParser()
+			const doc = parser.parseFromString(xml, 'text/xml')
+			const entries = doc.getElementsByTagName('entry')
+
+			Array.from(entries).forEach(entry => writeFile(entry))
+		})
 	} catch (error) {
 		console.log(error)
 	}
 }
 
-start()
+function start() {
+	try {
+		fs.readdir(input, 'utf8', (error, files) => {
+			if (error) throw error
+
+			files.forEach(file => readFile(file))
+		})
+	} catch (error) {
+		console.log(error)
+	}
+}
